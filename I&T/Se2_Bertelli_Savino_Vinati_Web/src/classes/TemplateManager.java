e46184d0447072a6de7d354df71ca9067be6405d
Matteo Savino <matteosavino98@gmail.com>
2022-01-05 12:43:12 +0100

    Implementation 0.0.2
    Implementation start

--- I&T/Se2_Bertelli_Savino_Vinati_Web/src/classes/TemplateManager.java
+++ I&T/Se2_Bertelli_Savino_Vinati_Web/src/classes/TemplateManager.java
@@ -1,6 +1,10 @@
 package classes;
 
+import java.io.IOException;
+
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -9,21 +13,24 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 public class TemplateManager {
 	private TemplateEngine templateEngine;
+	private HttpServletResponse response;
+	private final WebContext ctx;
 	
-	public TemplateManager(ServletContext servletContext, String path) {
+	public TemplateManager(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response) {
+		this.response = response;
 		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
 		templateResolver.setTemplateMode(TemplateMode.HTML);
 		this.templateEngine = new TemplateEngine();
 		this.templateEngine.setTemplateResolver(templateResolver);
 		templateResolver.setSuffix(".html");
+		ctx = new WebContext(request, response, servletContext, request.getLocale());
 	}
 	
-	/*
-	public function (ServletContext servletContext) {
-		ServletContext servletContext = getServletContext();
-		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-		ctx.setVariable("errorMsg", "Incorrect username or password");
-		path = "/index.html";
+	public void setVariable(String message, Object item) {
+		ctx.setVariable(message, item);
+	}
+	
+	public void redirect(String path) throws IOException {
 		templateEngine.process(path, ctx, response.getWriter());
-	}*/
+	}
 }
--- I&T/Se2_Bertelli_Savino_Vinati_Web/src/controllers/GoToLoginPage.java
+++ I&T/Se2_Bertelli_Savino_Vinati_Web/src/controllers/GoToLoginPage.java
@@ -2,23 +2,19 @@ package controllers;
 
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import classes.TemplateManager;
 
 
 @WebServlet("/GoToLoginPage")
 public class GoToLoginPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private TemplateEngine templateEngine;
+	private TemplateManager templateManager;
 
 	public GoToLoginPage() {
 		super();
@@ -31,9 +27,8 @@ public class GoToLoginPage extends HttpServlet {
 			throws ServletException, IOException {
 		
 		String path = "/index.html";
-		ServletContext servletContext = getServletContext();
-		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-		templateEngine.process(path, ctx, response.getWriter());
+		templateManager = new TemplateManager(getServletContext(), request, response);
+		templateManager.redirect(path);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
