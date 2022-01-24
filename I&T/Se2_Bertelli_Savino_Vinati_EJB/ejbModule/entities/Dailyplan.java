f5172a2cdfc63420c94d2f84f10481ff90f6fd60
jack <giacomo.vinati@mail.polimi.it>
2022-01-24 22:46:43 +0100

    Implementation 0.0.7
    Entities wip

---
+++ I&T/Se2_Bertelli_Savino_Vinati_EJB/ejbModule/entities/Dailyplan.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "dailyplan", schema="se2_bertelli_savino_vinati")
+@NamedQuery(name="Dailyplan.findAll", query="SELECT d FROM Dailyplan d")
+public class Dailyplan implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private Date date;
+	
+	@ManyToOne
+	@JoinColumn(name="idagronomist")
+	private User user;
+	
+	@OneToMany(mappedBy="dailyplan", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Visit> visits;
+
+	public Dailyplan() {
+		
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+}
---
+++ I&T/Se2_Bertelli_Savino_Vinati_EJB/ejbModule/entities/Visit.java
@@ -0,0 +1,79 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "visit", schema="se2_bertelli_savino_vinati")
+@NamedQuery(name="Visit.findAll", query="SELECT v FROM Visit v")
+public class Visit implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private Date datehour;
+
+	private String comment;
+	
+	private int state;
+
+	public Visit() {
+		
+	}
+	
+	@ManyToOne
+	@JoinColumn(name="iddailyplan")
+	private Dailyplan dailyplan;
+	
+	@ManyToOne
+	@JoinColumn(name="idfarm")
+	private Farm farm;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDatehour() {
+		return datehour;
+	}
+
+	public void setDatehour(Date datehour) {
+		this.datehour = datehour;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public int getIdfarm() {
+		return idfarm;
+	}
+
+	public void setIdfarm(int idfarm) {
+		this.idfarm = idfarm;
+	}
+
+
+
+}
